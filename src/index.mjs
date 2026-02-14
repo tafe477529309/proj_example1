236a5d734fb6733545b81fb68af3a8d66bc8f072
tafe477529309 <tafe477529309@gmail.com>
2026-02-14 13:33:44 +1000

    Initial Commit

---
+++ src/index.mjs
@@ -0,0 +1,8 @@
+import { add } from "./functions.mjs";
+
+console.log("Hello World!..");
+//console.log("Coding with Matt, how exciting");
+
+console.log("i'm in the middle of typing some code");
+
+console.log(add(20,5));
